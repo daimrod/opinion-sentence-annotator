8d734f55a73a3a1ae42e1ab2dee0db72c11e6ad4
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2016-07-28 15:23:07 +0200

    fix(main): Fix indentation

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ train = convert.read_dataset(data.semeval16_polarity_train)
 # Convert objective and neutral to objective/neutral
 for i in range(len(train.target_names)):
     if train.target_names[i] in ['objective',
-                                   'neutral',
-                                   'objective-OR-neutral']:
+                                 'neutral',
+                                 'objective-OR-neutral']:
         train.target_names[i] = 'objective/neutral'
 # Build the target array
 target = convert.strings_to_integers(train.target_names)
@@ -29,16 +29,16 @@ test = convert.read_dataset(data.semeval16_polarity_test)
 # Convert objective and neutral to objective/neutral
 for i in range(len(test.target_names)):
     if test.target_names[i] in ['objective',
-                               'neutral',
-                               'objective-OR-neutral']:
+                                'neutral',
+                                'objective-OR-neutral']:
         test.target_names[i] = 'objective/neutral'
 # Build the target array
 target = convert.strings_to_integers(test.target_names)
 test.target.extend(target)
 
 text_pipeline = Pipeline([('vect', CountVectorizer()),
-                     ('tfidf', TfidfTransformer()),
-                     ('clf', MultinomialNB()),
+                          ('tfidf', TfidfTransformer()),
+                          ('clf', MultinomialNB()),
 ])
 
 text_clf = text_pipeline.fit(train.data, train.target)
@@ -66,4 +66,3 @@ def old_pipeline(train):
 
     for doc, category in zip(docs_new, predicted):
         print('%r => %s' % (doc, train.target_names[category]))
-
