fc9a2fcfe207cc10228f8c16089691425cad4d83
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2016-07-28 16:18:44 +0200

    feat(main): Remove old code

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier
 from sklearn import metrics
 from sklearn.pipeline import Pipeline
-import numpy as np
+from sklearn.grid_search import GridSearchCV
 
 logger = logging.getLogger(__name__)
 
@@ -37,36 +37,23 @@ convert.merge_classes(test.target_names,
 target, labels = convert.strings_to_integers(test.target_names)
 test.target.extend(target)
 
-text_pipeline = Pipeline([('vect', CountVectorizer()),
-                          ('tfidf', TfidfTransformer()),
-                          ('clf', SGDClassifier(loss='hinge',
-                                                n_iter=5,
-                                                random_state=42)),
+parameters = {'vect__ngram_range': [(1, 1), (1, 2), (1, 3)],
+              'tfidf__use_idf': [True, False],
+              'clf__alpha': [1e-2, 1e-3, 1e-4, 1e-5],
+              'clf__n_iter': [1, 2, 5, 10],
+              'clf__loss': ['hinge', 'log', 'perceptron'],
+}
+pipeline = Pipeline([('vect', CountVectorizer()),
+                     ('tfidf', TfidfTransformer()),
+                     ('clf', SGDClassifier(loss='hinge')),
 ])
 
-text_clf = text_pipeline.fit(train.data, train.target)
-predicted = text_clf.predict(test.data)
+gs_clf = GridSearchCV(pipeline, parameters, n_jobs=6)
+gs_clf = gs_clf.fit(train.data, train.target)
+for param in gs_clf.best_params_:
+    print('%s: %r' % (param, gs_clf.best_params_[param]))
+
+predicted = gs_clf.predict(test.data)
 print(metrics.classification_report(test.target, predicted,
                                     target_names=labels))
 
-## DEPRECATED
-
-
-def old_pipeline(train):
-    # Tokenizing
-    count_vec = CountVectorizer()
-    X_train_counts = count_vec.fit_transform(train.data)
-
-    tfidf_transformer = TfidfTransformer(use_idf=True).fit(X_train_counts)
-    X_train_tfidf = tfidf_transformer.transform(X_train_counts)
-
-    clf = MultinomialNB().fit(X_train_tfidf, train.target)
-
-    docs_new = ['I\'m sad.', 'I\'m happy']
-    X_new_counts = count_vec.transform(docs_new)
-    X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-
-    predicted = clf.predict(X_new_tfidf)
-
-    for doc, category in zip(docs_new, predicted):
-        print('%r => %s' % (doc, train.target_names[category]))
