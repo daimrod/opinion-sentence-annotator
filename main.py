fdf251ea2b34ff98f98e9177b19d4883cfbc7657
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2016-10-07 15:53:00 +0200

    feat(main): Add logging facilities

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import importlib
 
 import codecs
 from collections import namedtuple
@@ -19,6 +20,8 @@ from sklearn.pipeline import FeatureUnion
 from sklearn.grid_search import GridSearchCV
 from sklearn.grid_search import ParameterGrid
 from sklearn.base import BaseEstimator, TransformerMixin
+import os
+import sys
 
 import happyfuntokenizing
 
@@ -26,10 +29,22 @@ from nltk.tokenize import TweetTokenizer
 
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+# StreamHandler
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)
+sh.setFormatter(formatter)
+logger.addHandler(sh)
+
+# FileHandler
+fh = logging.FileHandler('log.txt', 'a')
+fh.setFormatter(formatter)
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
 
 ## Configuration
-import os
-
 if 'DATA_DIR' in os.environ:
     DATA_DIR = os.environ['DATA_DIR']
 else:
@@ -425,38 +440,41 @@ def apply_pipeline(pipeline, train, test):
 
 ########## Pipeline
 def run(truncate=None):
+    logger.info('Read train dataset')
+    logger.debug(train_path)
     train = read_dataset(train_path)
-    # Convert objective and neutral to objective/neutral
+    logger.info('  Convert objective and neutral to objective/neutral')
     merge_classes(train.target_names,
                   ['objective',
                    'neutral',
                    'objective-OR-neutral'],
                   'neutral')
-    # Build the target array
+    logger.info('  Build the target array')
     target, labels = strings_to_integers(train.target_names)
     train.target.extend(target)
     train.data = train.data[:truncate]
     train.target = train.target[:truncate]
 
+    logger.info('Read test dataset')
+    logger.debug(test_path)
     test = read_dataset(test_path)
-    # Convert objective and neutral to objective/neutral
+    logger.info('  Convert objective and neutral to objective/neutral')
     merge_classes(test.target_names,
                   ['objective',
                    'neutral',
                    'objective-OR-neutral'],
                   'neutral')
-
-    # Build the target array
+    logger.info('  Build the target array')
     target, labels = strings_to_integers(test.target_names)
     test.target.extend(target)
 
-    # Tokenize text
+    logger.info('Tokenize text')
     for d in train.data:
         d['tok'] = happyfuntokenizer(d['text'])
     for d in test.data:
         d['tok'] = happyfuntokenizer(d['text'])
 
-    # Extract Senna
+    logger.info('Extract Senna features')
     senna = f_senna_multi([d['tok'] for d in train.data])
     for idx in range(len(train.data)):
         train.data[idx]['pos'] = senna[idx]['pos']
@@ -469,23 +487,7 @@ def run(truncate=None):
         test.data[idx]['chk'] = senna[idx]['chk']
         test.data[idx]['ner'] = senna[idx]['ner']
 
-    # parameters = {'vect__ngram_range': [(1, 1), (1, 2), (1, 3)],
-    #               'tfidf__use_idf': [True, False],
-    #               'clf__alpha': [1e-3, 1e-4, 1e-5],
-    #               'clf__n_iter': [1, 2, 5],
-    #               'clf__loss': ['hinge'],
-    # }
-
-    # parameters = {'tok__fun': [identity, happyfuntokenizer, nltktokenizer],
-
-    #               'features__ngram__vect__ngram_range': [(1, 3)],
-    #               'features__ngram__tfidf__use_idf': [True],
-
-    #               'clf__alpha': [1e-4],
-    #               'clf__n_iter': [5],
-    #               'clf__loss': ['hinge'],
-    #               }
-
+    logger.info('Train the pipeline')
     clf = Pipeline([
         ('text_features', FeatureUnion(
             [('vect', Pipeline([
@@ -511,20 +513,10 @@ def run(truncate=None):
         ('clf', SGDClassifier(loss='hinge',
                               n_iter=5,
                               random_state=42))]).fit(train.data, train.target)
+
+    logger.info('Classify test data')
     predicted = clf.predict(test.data)
-    print(metrics.classification_report(test.target, predicted,
-                                        target_names=list(set(test.target_names))))
-
-
-    # scorer = metrics.make_scorer(metrics.f1_score,
-    #                              average='micro')
-    # scorer = metrics.make_scorer(metrics.accuracy_score)
-    # scorer = 'accuracy'
-    # clf = GridSearchCV(pipeline, parameters, n_jobs=1,
-    #                    scoring=scorer, verbose=1)
-    # clf = clf.fit(train.data, train.target)
-    # for param in clf.best_params_:
-    #     print('%s: %r' % (param, clf.best_params_[param]))
-
-    # clf = pipeline
-    # clf = clf.fit(train.data, train.target)
+    logger.info('Results')
+    logger.debug(clf)
+    logger.info(metrics.classification_report(test.target, predicted,
+                                              target_names=list(set(test.target_names))))
