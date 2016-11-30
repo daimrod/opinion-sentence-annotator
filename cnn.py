fa851f63a4b59ba7975673af68d2e3d0ae74c4a8
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2016-11-30 15:11:23 +0100

    fix(cnn): Pre-built embeddings have their own shape, use it.

--- cnn.py
+++ cnn.py
@@ -224,6 +224,7 @@ class CNNChengGuo_Custom0(CNNChengGuo, emb.WithCustom0):
         logger.info('Preparing embedding matrix.')
         self.nb_words = min(self.max_nb_words, len(self.word_index))
         self.embeddings_index = {}
+        self.embedding_dim = self.custom0.syn0.shape[1]
         self.embedding_matrix = np.zeros((self.nb_words + 1,
                                           self.embedding_dim))
         for word, i in self.word_index.items():
