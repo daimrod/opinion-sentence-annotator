3d29cfb081b10db3a9dd110380d18a9473932ad0
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2016-12-02 10:25:51 +0100

    feat(run-embeddings): Small script to build embeddings.

---
+++ run-embeddings.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+
+if 'logger' not in locals():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s %(filename)s:%(lineno)s - %(funcName)20s() %(levelname)-8s %(message)s')
+    # StreamHandler
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.INFO)
+    sh.setFormatter(formatter)
+
+    # FileHandler
+    fh = logging.FileHandler('log.txt', 'a')
+    fh.setFormatter(formatter)
+    fh.setLevel(logging.DEBUG)
+
+    logger.handlers = [sh, fh]
+
+import embeddings as emb
+import resources as res
+import reader
+
+lexicon = reader.read_bing_liu(res.bing_liu_lexicon_path['negative'],
+                               res.bing_liu_lexicon_path['positive'])
+word2vec_param = {'workers': 6 }
+
+emb.get_custom1(word2vec_param=word2vec_param, lexicon=lexicon)
