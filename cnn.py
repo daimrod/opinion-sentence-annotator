39a31eb27ed6e90b3c5c79689a231c7a436f0707
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2017-01-04 17:07:36 +0100

    feat(cnn): Optimize on fmeasure

--- cnn.py
+++ cnn.py
@@ -37,6 +37,7 @@ from keras.layers import Conv1D, MaxPooling1D, Embedding
 from keras.layers import Convolution1D
 from keras.layers import Dropout, merge
 from keras.models import Model
+from keras.models import K
 from keras.optimizers import Adadelta
 from keras.optimizers import SGD
 
@@ -58,6 +59,58 @@ class TestEpoch(BaseLogger):
         self.pipeline.run_test()
         self.pipeline.print_results()
 
+
+def precision(y_true, y_pred):
+    '''Calculates the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    '''
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    '''Calculates the recall, a metric for multi-label classification of
+    how many relevant items are selected.
+    '''
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def fbeta_score(y_true, y_pred, beta=1):
+    '''Calculates the F score, the weighted harmonic mean of precision and recall.
+    This is useful for multi-label classification, where input samples can be
+    classified as sets of labels. By only using accuracy (precision) a model
+    would achieve a perfect score by simply assigning every class to every
+    input. In order to avoid this, a metric should penalize incorrect class
+    assignments as well (recall). The F-beta score (ranged from 0.0 to 1.0)
+    computes this, as a weighted mean of the proportion of correct class
+    assignments vs. the proportion of incorrect class assignments.
+    With beta = 1, this is equivalent to a F-measure. With beta < 1, assigning
+    correct classes becomes more important, and with beta > 1 the metric is
+    instead weighted towards penalizing incorrect class assignments.
+    '''
+    if beta < 0:
+        raise ValueError('The lowest choosable beta is zero (only precision).')
+
+    # If there are no true positives, fix the F score at 0 like sklearn.
+    if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
+        return 0
+
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    bb = beta ** 2
+    fbeta_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
+    return fbeta_score
+
+
+def fmeasure(y_true, y_pred):
+    '''Calculates the f-measure, the harmonic mean of precision and recall.
+    '''
+    return fbeta_score(y_true, y_pred, beta=1)
 
 class SaveBestModel(Callback):
     '''
@@ -259,7 +312,7 @@ class CNNBase(FullPipeline):
         self.model = Model(self.sequence_input, self.preds)
         self.model.compile(loss='categorical_crossentropy',
                            optimizer='rmsprop',
-                           metrics=['acc'])
+                           metrics=[fmeasure])
 
     def run_train(self):
         super().run_train()
