e96fb650bab89ffc962239eacc7d9b2c377294d8
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2017-01-12 13:22:25 +0100

    small test

--- test-theano.py
+++ test-theano.py
@@ -1,6 +1,9 @@
 # THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=gpu python test-theano.py
 # THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=cpu python test-theano.py
 
+import theano.sandbox.cuda
+theano.sandbox.cuda.use('gpu1')
+
 from theano import function, config, shared, sandbox
 import theano.tensor as T
 import numpy
