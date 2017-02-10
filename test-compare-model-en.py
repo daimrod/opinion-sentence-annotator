afaf943876584818a709bc2c5fc7c297ab423baa
Grégoire Jadi <gregoire.jadi@univ-nantes.fr>
2017-02-10 14:51:15 +0100

    test: Add test with new method

--- test-compare-model-en.py
+++ test-compare-model-en.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-
+import logger_config
 
 logger = logging.getLogger(__name__)
 
@@ -27,63 +27,105 @@ lexicons = [('bing_liu', bing_liu_lexicon),
             ('nrc_emotions', nrc_emotions_lexicon),
             ('mpqa', mpqa_lexicon)]
 
-model_name = 'model0'
-logger.info('Processing %s', model_name)
-model = emb.get_custom0(train_path=train_path)
-for (lex_test_name, lex_test) in lexicons:
-    logger.info('Compare %s on %s', model_name, lex_test_name)
-    emb.compare_model_with_lexicon(model, lex_test)
-    emb.compare_model_with_lexicon_class(model, lex_test)
+# model_name = 'model0'
+# logger.info('Processing %s', model_name)
+# model = emb.get_custom0(train_path=train_path)
+# for (lex_test_name, lex_test) in lexicons:
+#     logger.info('Compare %s on %s', model_name, lex_test_name)
+#     emb.compare_model_with_lexicon(model, lex_test)
+#     emb.compare_model_with_lexicon_class(model, lex_test)
 
-model_name = 'model1'
-logger.info('Processing %s', model_name)
-for lex_train_name, lex_train in lexicons:
-    logger.info('Training %s with %s', model_name, lex_train_name)
-    model = emb.get_custom1(train_path=train_path, lexicon=lex_train, suffix=lex_train_name)
-    for lex_test_name, lex_test in lexicons:
-        # Skip test lexicon if it's the same as the training lexicon
-        if lex_test_name == lex_train_name:
-            continue
-        logger.info('Compare %s on %s', model_name, lex_test_name)
-        emb.compare_model_with_lexicon(model, lex_test)
-        emb.compare_model_with_lexicon_class(model, lex_test)
-
-
-model_name = 'MCE'
-logger.info('Processing %s', model_name)
-for lex_train_name, lex_train in lexicons:
-    logger.info('Training %s with %s', model_name, lex_train_name)
-    model = emb.get_custom_mce(train_path=train_path, lexicon=lex_train, suffix=lex_train_name)
-    for lex_test_name, lex_test in lexicons:
-        # Skip test lexicon if it's the same as the training lexicon
-        if lex_test_name == lex_train_name:
-            continue
-        logger.info('Compare %s on %s', model_name, lex_test_name)
-        emb.compare_model_with_lexicon(model, lex_test)
-        emb.compare_model_with_lexicon_class(model, lex_test)
+# model_name = 'model1'
+# logger.info('Processing %s', model_name)
+# for lex_train_name, lex_train in lexicons:
+#     logger.info('Training %s with %s', model_name, lex_train_name)
+#     model = emb.get_custom1(train_path=train_path, lexicon=lex_train, suffix=lex_train_name)
+#     for lex_test_name, lex_test in lexicons:
+#         # Skip test lexicon if it's the same as the training lexicon
+#         if lex_test_name == lex_train_name:
+#             continue
+#         logger.info('Compare %s on %s', model_name, lex_test_name)
+#         emb.compare_model_with_lexicon(model, lex_test)
+#         emb.compare_model_with_lexicon_class(model, lex_test)
 
 
-model_name = 'faruqui'
-logger.info('Processing %s', model_name)
-for lex_train_name, lex_train in lexicons:
-    logger.info('Training %s with %s', model_name, lex_train_name)
-    model = emb.get_custom0(train_path=train_path)
-    model = emb.build_custom3(model, lexicon=lex_train)
-    for lex_test_name, lex_test in lexicons:
-        # Skip test lexicon if it's the same as the training lexicon
-        if lex_test_name == lex_train_name:
-            continue
-        logger.info('Compare %s on %s', model_name, lex_test_name)
-        emb.compare_model_with_lexicon(model, lex_test)
-        emb.compare_model_with_lexicon_class(model, lex_test)
+# model_name = 'MCE'
+# logger.info('Processing %s', model_name)
+# for lex_train_name, lex_train in lexicons:
+#     logger.info('Training %s with %s', model_name, lex_train_name)
+#     model = emb.get_custom_mce(train_path=train_path, lexicon=lex_train, suffix=lex_train_name)
+#     for lex_test_name, lex_test in lexicons:
+#         # Skip test lexicon if it's the same as the training lexicon
+#         if lex_test_name == lex_train_name:
+#             continue
+#         logger.info('Compare %s on %s', model_name, lex_test_name)
+#         emb.compare_model_with_lexicon(model, lex_test)
+#         emb.compare_model_with_lexicon_class(model, lex_test)
 
 
-model_name = 'faruqui++'
+# model_name = 'faruqui'
+# logger.info('Processing %s', model_name)
+# for lex_train_name, lex_train in lexicons:
+#     logger.info('Training %s with %s', model_name, lex_train_name)
+#     model = emb.get_custom0(train_path=train_path)
+#     model = emb.build_custom3(model, lexicon=lex_train)
+#     for lex_test_name, lex_test in lexicons:
+#         # Skip test lexicon if it's the same as the training lexicon
+#         if lex_test_name == lex_train_name:
+#             continue
+#         logger.info('Compare %s on %s', model_name, lex_test_name)
+#         emb.compare_model_with_lexicon(model, lex_test)
+#         emb.compare_model_with_lexicon_class(model, lex_test)
+
+
+# model_name = 'faruqui++'
+# logger.info('Processing %s', model_name)
+# for lex_train_name, lex_train in lexicons:
+#     logger.info('Training %s with %s', model_name, lex_train_name)
+#     model = emb.get_custom0(train_path=train_path)
+#     model = emb.build_custom3_1(model, lexicon=lex_train)
+#     for lex_test_name, lex_test in lexicons:
+#         # Skip test lexicon if it's the same as the training lexicon
+#         if lex_test_name == lex_train_name:
+#             continue
+#         logger.info('Compare %s on %s', model_name, lex_test_name)
+#         emb.compare_model_with_lexicon(model, lex_test)
+#         emb.compare_model_with_lexicon_class(model, lex_test)
+
+model_name = 'faruqui3_1'
 logger.info('Processing %s', model_name)
 for lex_train_name, lex_train in lexicons:
     logger.info('Training %s with %s', model_name, lex_train_name)
     model = emb.get_custom0(train_path=train_path)
     model = emb.build_custom3_1(model, lexicon=lex_train)
+    for lex_test_name, lex_test in lexicons:
+        # Skip test lexicon if it's the same as the training lexicon
+        if lex_test_name == lex_train_name:
+            continue
+        logger.info('Compare %s on %s', model_name, lex_test_name)
+        emb.compare_model_with_lexicon(model, lex_test)
+        emb.compare_model_with_lexicon_class(model, lex_test)
+
+model_name = 'faruqui3_2 d=1'
+logger.info('Processing %s', model_name)
+for lex_train_name, lex_train in lexicons:
+    logger.info('Training %s with %s', model_name, lex_train_name)
+    model = emb.get_custom0(train_path=train_path)
+    model = emb.build_custom3_2(model, lexicon=lex_train, d=1)
+    for lex_test_name, lex_test in lexicons:
+        # Skip test lexicon if it's the same as the training lexicon
+        if lex_test_name == lex_train_name:
+            continue
+        logger.info('Compare %s on %s', model_name, lex_test_name)
+        emb.compare_model_with_lexicon(model, lex_test)
+        emb.compare_model_with_lexicon_class(model, lex_test)
+
+model_name = 'faruqui3_2 d=0.1'
+logger.info('Processing %s', model_name)
+for lex_train_name, lex_train in lexicons:
+    logger.info('Training %s with %s', model_name, lex_train_name)
+    model = emb.get_custom0(train_path=train_path)
+    model = emb.build_custom3_2(model, lexicon=lex_train, d=0.1)
     for lex_test_name, lex_test in lexicons:
         # Skip test lexicon if it's the same as the training lexicon
         if lex_test_name == lex_train_name:
